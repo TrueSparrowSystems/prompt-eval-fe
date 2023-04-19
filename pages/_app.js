4c990d2be331fb5b40dd356a1c6b9a5c7f5f30f0
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-04-19 12:41:36 +0530

    Updated fonts

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,17 @@ import store from "../store/store";
 import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ApolloProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ theme.js
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Inter', sans-serif;`,
+  },
+});
+
+export default theme;

c4230719941c06a3f7dba785b18a9a37089ef4cb
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-04-20 15:01:29 +0530

    Integration fixes

--- components/Experiments/LeftSideBar/ExperimentList.js
+++ components/Experiments/LeftSideBar/ExperimentList.js
@@ -3,35 +3,33 @@ import ExperimentCell from "./ExperimentCell";
 import { useQuery } from "@apollo/client";
 import Queries from "../../../queries/Queries";
 import ExperimentListSkeleton from "../../Skeletons/ExperimentListSkeleton";
-import {useExpContext} from "../../../context/ExpContext";
+import { useExpContext } from "../../../context/ExpContext";
 
 export default function ExperimentList() {
   const [selectedExperiment, setSelectedExperiment] = useState(0);
   const { data, loading, error } = useQuery(Queries.experimentList);
-  const { setSelectedExperimentInfo  } = useExpContext();
+  const { setSelectedExperimentInfo } = useExpContext();
 
   const handleChange = (index) => {
     setSelectedExperiment(index);
     setSelectedExperimentInfo(data?.experimentList[index]);
-    
   };
-  
-  useEffect(() => {
-    
-    handleChange(selectedExperiment);
 
+  useEffect(() => {
+    handleChange(selectedExperiment);
   }, []);
 
   if (loading) {
-   return  <ExperimentListSkeleton />;
+    return <ExperimentListSkeleton />;
   }
 
   if (error) {
     return null;
   }
+  if (data?.experimentList.length !== 0) {
+    setSelectedExperimentInfo(data?.experimentList[0]);
+  }
 
-  
-  
   return (
     <div
       className="mt-[20px] second-step"

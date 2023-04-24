b6595585eb38374e05f115a06241962b9a8aa928
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-04-24 14:07:18 +0530

    Fix

--- components/Experiments/RightSideBar/Report/Report.js
+++ components/Experiments/RightSideBar/Report/Report.js
@@ -66,9 +66,9 @@ function Report(props) {
     <div
       style={{
         background: " #ffffff",
-        "box-shadow":
+        "boxShadow":
           "0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),0px 2px 1px -1px rgba(0, 0, 0, 0.2)",
-        " border-radius": " 8px;",
+        " borderRadius": "8px",
       }}
     >
       <div className="flex items-center gap-[10px] cursor-pointer hover:opacity-80 opacity-60 px-[30px]">

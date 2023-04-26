4b4b23cd612a82163350c6ca9e307cb8d5867993
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-04-26 15:42:37 +0530

    Added toast msg

--- components/Experiments/RightSideBar/PromptTemplate/ClonePromptTemplate.js
+++ components/Experiments/RightSideBar/PromptTemplate/ClonePromptTemplate.js
@@ -7,6 +7,12 @@ import { useExpContext } from "../../../../context/ExpContext";
 import { useMutation } from "@apollo/client";
 import Queries from "../../../../queries/Queries";
 import UpdateTemplateSkeleton from "../../../Skeletons/UpdateTemplateSkeleton";
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function ClonePromptTemplate() {
   const isCloned = useRef(false);
@@ -37,50 +43,64 @@ function ClonePromptTemplate() {
       {loading ? (
         <UpdateTemplateSkeleton />
       ) : (
-        <div className={`${styles.experimentBox}`}>
-          <div
-            className="flex items-center gap-[10px] cursor-pointer hover:opacity-80 opacity-60"
-            onClick={() => {
-              setShowClone(false);
+        <>
+          <Snackbar
+            open={open}
+            autoHideDuration={5000}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "center",
             }}
           >
-            <BackArrow />
-            <div className="text-[14px] opacity-60 py-[25px]">
-              {promptTemplate.name}
-            </div>
-          </div>
-          <input
-            className="text-[20px] font-bold opacity-60 outline-none pb-[25px] w-1/3"
-            type="text"
-            value={templateName}
-            onChange={(e) => {
-              setTemplateName(e.target.value);
-            }}
-          />
-          {promptTemplate.conversation.map((chat) => (
-            <div className="flex p-2">
-              <div className="uppercase cursor-pointer text-md hover:bg-[#fff] p-[10px] h-[40px] basis-20">
-                {chat.role}
-              </div>
-              <textarea
-                className={`w-full border rounded-[4px] h-[120px] p-[10px] ml-[40px] mr-[10px] outline-none`}
-                value={chat.content}
-              />
-            </div>
-          ))}
-          <div>
-            <Button
-              variant="contained"
-              style={{
-                background: "#2196F3",
-                border: "1px solid rgba(0, 0, 0, 0.23)",
+            <Alert severity="success" sx={{ width: "100%" }}>
+              {promptTemplate.name} has beed cloned successfully.
+            </Alert>
+          </Snackbar>
+          <div className={`${styles.experimentBox}`}>
+            <div
+              className="flex items-center gap-[10px] cursor-pointer hover:opacity-80 opacity-60"
+              onClick={() => {
+                setShowClone(false);
               }}
-              sx={{ ml: "10px", textTransform: "none" }}
             >
-              Save
-            </Button>
+              <BackArrow />
+              <div className="text-[14px] opacity-60 py-[25px]">
+                {promptTemplate.name}
+              </div>
+            </div>
+            <input
+              className="text-[20px] font-bold opacity-60 outline-none pb-[25px] w-1/3"
+              type="text"
+              value={templateName}
+              onChange={(e) => {
+                setTemplateName(e.target.value);
+              }}
+            />
+            {promptTemplate.conversation.map((chat) => (
+              <div className="flex p-2">
+                <div className="uppercase cursor-pointer text-md hover:bg-[#fff] p-[10px] h-[40px] basis-20">
+                  {chat.role}
+                </div>
+                <textarea
+                  className={`w-full border rounded-[4px] h-[120px] p-[10px] ml-[40px] mr-[10px] outline-none`}
+                  value={chat.content}
+                />
+              </div>
+            ))}
+            <div>
+              <Button
+                variant="contained"
+                style={{
+                  background: "#2196F3",
+                  border: "1px solid rgba(0, 0, 0, 0.23)",
+                }}
+                sx={{ ml: "10px", textTransform: "none" }}
+              >
+                Save
+              </Button>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );

5986204063b0ce229fd30b5adf0606588c0e6c9e
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-04-27 18:05:05 +0530

    Removed the edit icon and made cell clickable

--- components/Experiments/RightSideBar/PromptTemplate/PromptTemplateCells.js
+++ components/Experiments/RightSideBar/PromptTemplate/PromptTemplateCells.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import ViewReportArrow from "../../../../assets/Svg/ViewReportArrow";
 import Clone from "../../../../assets/Svg/Clone";
-import Edit from "../../../../assets/Svg/Edit";
 import Calendar from "../../../../assets/Svg/Calendar";
 import RunModal from "./RunModal";
 import { getFormattedDate } from "../../../../utils/DateFormates";
@@ -48,84 +47,85 @@ function PromptTemplateCells({ PromptTemplate }) {
   }
 
   return (
-    <div className={`flex items-center text-md border-b-2`}>
-      <div className={`basis-1/5 border-r-2 px-[10px] py-[44px] mr-[10px]`}>
-        {PromptTemplate.name}
-      </div>
-      <div className="basis-1/5 px-[10px]">
-        {PromptTemplate.latestEvaluationReport[0] !== null
-          ? PromptTemplate.latestEvaluationReport[0].accuracy + "%"
-          : "--"}
-      </div>
-      <div className="basis-1/5 px-[10px]">
-        {PromptTemplate.latestEvaluationReport[0] !== null
-          ? PromptTemplate.latestEvaluationReport[0].model
-          : "--"}
-      </div>
-      <div className="basis-1/5 px-[10px]">
-        <div className="flex items-center gap-[10px]">
-          <Calendar />
-          <div>
-            {PromptTemplate.createdAt &&
-              getFormattedDate(PromptTemplate.createdAt)}
-          </div>
+    <>
+      <div
+        className={`flex items-center text-md border-b-2 cursor-pointer`}
+        onClick={() => {
+          setPromptTemplate(PromptTemplate);
+          setShowClone(false);
+          setShowEdit(true);
+        }}
+      >
+        <div className={`basis-1/5 border-r-2 px-[10px] py-[44px] mr-[10px]`}>
+          {PromptTemplate.name}
         </div>
-        <div
-          className={`flex items-center gap-[10px] mt-[10px] ${
-            PromptTemplate.latestEvaluationReport[0] !== null
-              ? "cursor-pointer"
-              : ""
-          }`}
-          onClick={() => {
-            if (PromptTemplate.latestEvaluationReport[0] !== null) {
-              setShowReport(true);
-              setReportId(PromptTemplate.latestEvaluationReport[0].id);
-            }
-          }}
-        >
-          <div className="underline">View Report</div>
-          <div>
-            <ViewReportArrow />
-          </div>
+        <div className="basis-1/5 px-[10px]">
+          {PromptTemplate.latestEvaluationReport[0] !== null
+            ? PromptTemplate.latestEvaluationReport[0].accuracy + "%"
+            : "--"}
         </div>
-      </div>
-
-      <div className="basis-1/5 flex items-center justify-around px-[10px]">
-        <div>
-          <Button
-            variant="outlined"
-            sx={{ textTransform: "none" }}
+        <div className="basis-1/5 px-[10px]">
+          {PromptTemplate.latestEvaluationReport[0] !== null
+            ? PromptTemplate.latestEvaluationReport[0].model
+            : "--"}
+        </div>
+        <div className="basis-1/5 px-[10px]">
+          <div className="flex items-center gap-[10px]">
+            <Calendar />
+            <div>
+              {PromptTemplate.createdAt &&
+                getFormattedDate(PromptTemplate.createdAt)}
+            </div>
+          </div>
+          <div
+            className={`flex items-center gap-[10px] mt-[10px] z-10 ${
+              PromptTemplate.latestEvaluationReport[0] !== null
+                ? "cursor-pointer"
+                : ""
+            }`}
             onClick={(e) => {
-              setShowRunModal(!showRunModal);
+              e.stopPropagation();
+              if (PromptTemplate.latestEvaluationReport[0] !== null) {
+                setShowReport(true);
+                setReportId(PromptTemplate.latestEvaluationReport[0].id);
+              }
             }}
           >
-            Run
-          </Button>
-          <RunModal
-            showRunModal={showRunModal}
-            setShowRunModal={setShowRunModal}
-          />
-        </div>
-        <div className="flex items-center gap-[20px]">
-          <div
-            onClick={() => {
-              handleClone();
-            }}
-          >
-            <Clone />
+            <div className="underline">View Report</div>
+            <div>
+              <ViewReportArrow />
+            </div>
           </div>
-          <div
-            onClick={() => {
-              setPromptTemplate(PromptTemplate);
-              setShowClone(false);
-              setShowEdit(true);
-            }}
-          >
-            <Edit />
+        </div>
+
+        <div className="basis-1/5 flex items-center justify-around px-[10px] z-10">
+          <div>
+            <Button
+              variant="outlined"
+              className="z-10"
+              sx={{ textTransform: "none" }}
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowRunModal(!showRunModal);
+              }}
+            >
+              Run
+            </Button>
+          </div>
+          <div className="flex items-center gap-[20px]">
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                handleClone();
+              }}
+            >
+              <Clone />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <RunModal showRunModal={showRunModal} setShowRunModal={setShowRunModal} />
+    </>
   );
 }
 

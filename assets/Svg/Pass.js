8758841df082f434bf177521b0ba72c123f5a1d9
Khushi Jain <jainkhushi2403@gmail.com>
2023-05-11 18:53:47 +0530

    converted svg to react

--- assets/Svg/Pass.js
+++ assets/Svg/Pass.js
@@ -3,20 +3,20 @@ import React from "react";
 function Pass(props) {
   return (
     <svg
-      width="14"
-      height="14"
-      viewBox="0 0 14 14"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9.4001 5.4001L5.82034 8.6001L4.6001 7.50931M13.4001 3.0001L13.4001 11.0002C13.4001 12.3257 12.3256 13.4002 11.0001 13.4002H3.0001C1.67461 13.4002 0.600098 12.3257 0.600098 11.0002V3.0001C0.600098 1.67461 1.67461 0.600098 3.0001 0.600098H11.0001C12.3256 0.600098 13.4001 1.67461 13.4001 3.0001Z"
-        stroke="#2E7D32"
-        stroke-width="1.2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+    xmlns="http://www.w3.org/2000/svg"
+    width={14}
+    height={14}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#2E7D32"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.2}
+      d="M9.4 5.4 5.82 8.6 4.6 7.51M13.4 3v8a2.4 2.4 0 0 1-2.4 2.4H3A2.4 2.4 0 0 1 .6 11V3A2.4 2.4 0 0 1 3 .6h8A2.4 2.4 0 0 1 13.4 3Z"
+    />
+  </svg>
   );
 }
 

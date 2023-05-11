2c54b7e2756147360b149cc7a51a03a00c3aa443
Khushi Jain <jainkhushi2403@gmail.com>
2023-05-11 19:16:26 +0530

    test creation flow changed

--- components/Experiments/RightSideBar/TestCases/TestCases.js
+++ components/Experiments/RightSideBar/TestCases/TestCases.js
@@ -18,31 +18,24 @@ export default function TestCases() {
     variables: { experimentId: selectedExperimentInfo?.id },
   });
 
-  const [
-    createTestCases,
-    {
-      data: createTestCase,
-      loading: loadingCreateTestCase,
-      error: errorCreateTestCase,
-    },
-  ] = useMutation(Queries.createTestCases);
-
   const { addTestCase, setAddTestCase, setAddDynamicVars, setShowEmptyState } =
     useCompSelectorContext();
 
   const [unsavedChanges, setUnsavedChanges] = useState(false);
 
   useEffect(() => {
-    if (addTestCase) {
-      handleAddTestCase();
-      setAddTestCase(false);
-    }
     if (
       (testCase == null || Object.keys(testCase).length == 0) &&
       data?.testCases.length > 0
     )
       setTestCase(data?.testCases[0]);
-  }, [addTestCase, data, createTestCase]);
+
+  }, [data]);
+
+  useEffect(() => {
+    if(!addTestCase)
+    refetch();
+  }, [addTestCase]);
 
   useEffect(() => {
     setTestCase(data?.testCases[0]);
@@ -53,40 +46,11 @@ export default function TestCases() {
     refetch();
   }, []);
 
-  const handleAddTestCase = async () => {
-    try {
-      let allDynamicVars = {};
-      if (selectedExperimentInfo?.dynamicVariables) {
-        for (let i = 0;i < selectedExperimentInfo?.dynamicVariables.length;i++) {
-          allDynamicVars[selectedExperimentInfo?.dynamicVars[i]] = "";
-        }
-      }
-      await createTestCases({
-        variables: {
-          name: "Untitled Test Case",
-          description: "Initial Test Case Description",
-          dynamicVarValues: JSON.stringify(allDynamicVars),
-          experimentId: selectedExperimentInfo?.id,
-        },
-      });
-      await refetch();
-    } catch (err) {
-      console.log(err);
-      return err;
-    }
-  };
-
   if (data?.testCases.length === 0) setShowEmptyState(true);
 
   return (
     <div>
-      {createTestCase && (
-        <Toast msg={MESSAGES.TEST_CASE.CREATED} type="success" />
-      )}
-      {errorCreateTestCase && (
-        <Toast msg={MESSAGES.TEST_CASE.FAILED} type="error" />
-      )}
-      {loading || data?.testCases.length === 0 ? (
+      {!addTestCase && (loading || data?.testCases.length === 0) ? (
         <EmptyState />
       ) : (
         <div className={`flex ${styles.experimentBox}`}>
@@ -97,7 +61,7 @@ export default function TestCases() {
           ) : (
             <>
               <div className={`basis-64 ${styles.subBoxHeight}`}>
-                <TestCasesList data={data} unsavedChanges={unsavedChanges} />
+                <TestCasesList data={data} unsavedChanges={unsavedChanges}/>
               </div>
               <div className="mt-[13px] w-full ">
                 <TestCaseTabs

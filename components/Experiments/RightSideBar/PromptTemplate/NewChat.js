78a5786dc8afb7a2010c877a9c37b9e0a95bb8a8
Khushi Jain <jainkhushi2403@gmail.com>
2023-05-18 01:57:36 +0530

    Placeholder change for template

--- components/Experiments/RightSideBar/PromptTemplate/NewChat.js
+++ components/Experiments/RightSideBar/PromptTemplate/NewChat.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import RemoveIcon from "../../../../assets/Svg/RemoveIcon";
 
-function NewChat({ prompt, remove, selectedChat, setSelectedChat, unsavedChanges, setUnsavedChanges }) {
+function NewChat({
+  prompt,
+  remove,
+  selectedChat,
+  setSelectedChat,
+  unsavedChanges,
+  setUnsavedChanges,
+}) {
   const [isHover, setIsHover] = useState(false);
   const [ptomptMessage, setPromptMessage] = useState(prompt.content);
   const [promptRole, setPromptRole] = useState(prompt.role);
 
   return (
     <li
-      className={`flex ${isHover || selectedChat===prompt.id?"bg-[#F8FAFB]":""} p-2`}
+      className={`flex ${
+        isHover || selectedChat === prompt.id ? "bg-[#F8FAFB]" : ""
+      } p-2`}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
@@ -25,8 +34,10 @@ function NewChat({ prompt, remove, selectedChat, setSelectedChat, unsavedChanges
       </div>
       <textarea
         className={`w-full border rounded-[4px] h-[120px] p-[10px] ml-[40px] mr-[10px] outline-none ${
-          (isHover || selectedChat===prompt.id)? "border-[#2196F380] bg-[#F8FAFB]" : "border-[#0000001A]"
-        } resize-none`}
+          isHover || selectedChat === prompt.id
+            ? "border-[#2196F380] bg-[#F8FAFB]"
+            : "border-[#0000001A]"
+        }`}
         value={ptomptMessage}
         onChange={(e) => {
           if (!unsavedChanges) setUnsavedChanges(true);
@@ -34,10 +45,10 @@ function NewChat({ prompt, remove, selectedChat, setSelectedChat, unsavedChanges
         }}
         onBlur={(e) => {
           prompt.content = e.target.value;
-          setSelectedChat(null)
+          setSelectedChat(null);
         }}
         onClick={() => setSelectedChat(prompt.id)}
-        placeholder="Define template variables in {‘variable_name’} format within the prompt."
+        placeholder="Define template variables in {{variable_name}} format within the prompt."
       />
       <div
         className={`cursor-pointer ${

af8f79444534518a707e46594260983b5dec69b0
Khushi Jain <jainkhushi2403@gmail.com>
2023-05-25 20:27:02 +0530

    removed caching condition

--- apollo-client.js
+++ apollo-client.js
@@ -2,13 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_API_BASE_URL,
-    cache: new InMemoryCache({
-        typePolicies: {
-            PromptTemplateType: {
-              keyFields: ["updatedAt"],
-            },
-          },
-    }),
+    cache: new InMemoryCache(),
 });
 
 export default client;

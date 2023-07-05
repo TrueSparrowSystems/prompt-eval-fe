fa204d04fb6c0cb12623b18f7862e75c2c48546c
Khushi Jain <jainkhushi2403@gmail.com>
2023-07-05 16:58:55 +0530

    Auth updated

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,5 +1,3 @@
-export default function handler(_, res) {
-    res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
-    res.statusCode = 401
-    res.end(`Auth Required.`)
-  }
+import { createApiPage } from 'nextjs-basic-auth-middleware'
+
+export default createApiPage();

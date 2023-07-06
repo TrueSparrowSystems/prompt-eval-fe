c0f4a13e85c537f585548ff3f4feb927afeacc5a
Khushi Jain <69646098+khushij12@users.noreply.github.com>
2023-07-06 16:18:17 +0530

    Error message show (#168)

--- middleware.js
+++ middleware.js
@@ -1,5 +1,10 @@
 import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-const auth = createNextAuthMiddleware();
+const cred = process.env.BASIC_AUTH_CREDENTIALS || "";
+
+const auth =
+  cred.length === 0
+    ? createNextAuthMiddleware()
+    : createNextAuthMiddleware({ users: [{ user: "test", password: "test" }] });
 
 export default auth;
